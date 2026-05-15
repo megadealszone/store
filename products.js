06505b832420268d9debb6558975c7127f5e39ec
Mega Deals Zone <mdromanislamhabib@gmail.com>
2026-05-15 15:54:01 +0600

    product.html

--- products.js
+++ products.js
@@ -1,1 +1,14 @@
-
+const products = [
+  {
+    title: "Wireless Headphone",
+    price: "$25.99",
+    image: "https://via.placeholder.com/300",
+    link: "https://amazon.com"
+  },
+  {
+    title: "Smart Watch",
+    price: "$39.99",
+    image: "https://via.placeholder.com/300",
+    link: "https://amazon.com"
+  }
+];
